7cb377086f2d62bba39a1b51b42a8765c11054c8
skyguy94 <skyguy94@gmail.com>
2016-02-16 21:31:58 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # Simple.LocalDb
-Simple Managed wrapper around the unmanaged LocalDb API.
+
+Simple.LocalDb is a simple managed wrapper around the LocalDB API. There's quite a few LocalDb wrappers and frameworks that offer many features. 
+
+This framework has no features. It does not have error checking. It does not do anything smart. It doesn't try to help you in any way. There are no unit tests. There are no integration tests. There are no interfaces, inheritance, coupling, or clever abstractions.
+
+If you're like me and you basically want to call 'CreateInstance' from your test code, this package is your friend.
+
+If you need to automagically deploy a dacpac to a dynamically created test instance, this is not your package.
+
+If you want a smart and robust LocalDb provider that can create different versions of instances, be localized, will clean up cruft localdb files from the filesystem, and will recover from failures, this is not your package.
+
+Try one of these instead:
+
+ - [Nicholas Ham's LocalDb NuGet package](https://www.nuget.org/packages/SqlLocalDb.Dac/).
+ -  [Martin Costello's  LocalDb NuGet package](https://www.nuget.org/packages/SqlLocalDb.Dac/).
+
+To use this package:
+
+- You can create an instance of the managed interface, which hides some of the marshaling complexity from you (there's not much).
+
+- You can create an instance of the unmanaged interface and write your own better wrapper around it.
+
+- If you want to use it as a nuget package. Have at it.
+
+- If you want to include the project source in your project. Have at it.
+
+- If you want to copy paste parts of the code into yours, have at it.
+
+- If you need to target the .Net 3.5 (neee 2.0) runtime and reference a signed assembly. Grab the sample and have at it.
+- If you want to copy this code and publish a new nuget package from it with 0..n modifications. Have at it.
+
+Example Managed API usage:
+
+```
+//New up the API.
+var api = new ManagedLocalDbApi();
+
+//Get all the instances as a list of strings.
+var instanceNames = api.GetInstanceNames().ToList();
+
+//Create a new instance with a random name.
+var testInstanceName = Path.GetRandomFileName();
+api.CreateInstance(testInstanceName);
+
+//Start the instance and get its connection string (named pipes).
+var connection = api.StartInstance(testInstanceName);
+
+//Get a bunch of fun details about the instance.
+var testInstance = api.GetInstance(testInstanceName);
+
+//Stop the instance. Timeout if it takes longer than 60 seconds.
+api.StopInstance(testInstanceName, TimeSpan.FromSeconds(60));
+
+//Delete the instance.
+api.DeleteInstance(testInstanceName);
+```
+
+
---
+++ Simple.LocalDb/UnmanagedLocalDbApi.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+using FILETIME = System.Runtime.InteropServices.ComTypes.FILETIME;
+
+namespace SqlLocalDbTests
+{
+  [SuppressMessage("ReSharper", "IdentifierTypo")]
+  [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
+  public class UnmanagedLocalDbApi
+  {
+    private readonly IntPtr _api;
+    public UnmanagedLocalDbApi()
+    {
+      string dllName = GetLocalDbDllName();
+      if (dllName == null) throw new InvalidOperationException("Could not find local db dll.");
+
+      _api = Kernel32.LoadLibraryEx(dllName, IntPtr.Zero, Kernel32.LoadLibraryFlags.LoadLibrarySearchDefaultDirs);
+      if (_api == IntPtr.Zero) throw new Win32Exception();
+
+      CreateInstance = GetFunction<LocalDBCreateInstance>();
+      DeleteInstance = GetFunction<LocalDBDeleteInstance>();
+      FormatMessage = GetFunction<LocalDBFormatMessage>();
+      GetInstanceInfo = GetFunction<LocalDBGetInstanceInfo>();
+      GetInstances = GetFunction<LocalDBGetInstances>();
+      GetVersionInfo = GetFunction<LocalDBGetVersionInfo>();
+      GetVersions = GetFunction<LocalDBGetVersions>();
+      ShareInstance = GetFunction<LocalDBShareInstance>();
+      StartInstance = GetFunction<LocalDBStartInstance>();
+      StartTracing = GetFunction<LocalDBStartTracing>();
+      StopInstance = GetFunction<LocalDBStopInstance>();
+      StopTracing = GetFunction<LocalDBStopTracing>();
+      UnshareInstance = GetFunction<LocalDBUnshareInstance>();
+    }
+    public string ApiVersion { get; private set; }
+    public const int MaxPath = 260;
+    public const int MaxName = 129;
+    public const int MaxSid = 187;
+
+    public LocalDBCreateInstance CreateInstance;
+    public LocalDBDeleteInstance DeleteInstance;
+    public LocalDBFormatMessage FormatMessage;
+    public LocalDBGetInstanceInfo GetInstanceInfo;
+    public LocalDBGetInstances GetInstances;
+    public LocalDBGetVersionInfo GetVersionInfo;
+    public LocalDBGetVersions GetVersions;
+    public LocalDBShareInstance ShareInstance;
+    public LocalDBStartInstance StartInstance;
+    public LocalDBStartTracing StartTracing;
+    public LocalDBStopInstance StopInstance;
+    public LocalDBStopTracing StopTracing;
+    public LocalDBUnshareInstance UnshareInstance;
+
+    private string GetLocalDbDllName()
+    {
+      bool isWow64Process = Environment.Is64BitOperatingSystem && !Environment.Is64BitProcess;
+      var registryView = isWow64Process ? RegistryView.Registry32 : RegistryView.Default;
+      using (var rootKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+      {
+        var versions = rootKey.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server Local DB\Installed Versions");
+        if (versions == null) throw new InvalidOperationException("LocalDb not installed.");
+
+        var latest = versions.GetSubKeyNames().Select(s => new Version(s)).OrderBy(s => s).FirstOrDefault();
+        if (latest == null) throw new InvalidOperationException("LocalDb not installed.");
+        using (var versionKey = versions.OpenSubKey(latest.ToString()))
+        {
+          ApiVersion = latest.ToString();
+          var path = (string)versionKey?.GetValue("InstanceAPIPath");
+          return path;
+        }
+      }
+    }
+
+    private T GetFunction<T>() where T : class
+    {
+      string name = typeof(T).Name;
+      var ptr = Kernel32.GetProcAddress(_api, name);
+      if (ptr == IntPtr.Zero) throw new EntryPointNotFoundException($@"{name}");
+
+      object function = Marshal.GetDelegateForFunctionPointer(ptr, typeof(T));
+      return (T)function;
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBCreateInstance([MarshalAs(UnmanagedType.LPWStr)] string wszVersion, [MarshalAs(UnmanagedType.LPWStr)] string pInstanceName, int dwFlags);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBDeleteInstance([MarshalAs(UnmanagedType.LPWStr)] string pInstanceName, int dwFlags);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBFormatMessage(int hrLocalDB, int dwFlags, int dwLanguageId, [MarshalAs(UnmanagedType.LPWStr)][Out] StringBuilder wszMessage, ref int lpcchMessage);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBGetInstanceInfo([MarshalAs(UnmanagedType.LPWStr)] string wszInstanceName, ref LocalDbInstanceInfo pInstanceInfo, int dwInstanceInfoSize);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBGetInstances(IntPtr pInstanceNames, ref int lpdwNumberOfInstances);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBGetVersionInfo([MarshalAs(UnmanagedType.LPWStr)] string wszVersionName, IntPtr pVersionInfo, int dwVersionInfoSize);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBGetVersions(IntPtr pVersion, ref int lpdwNumberOfVersions);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBShareInstance(IntPtr pOwnerSid, [MarshalAs(UnmanagedType.LPWStr)] string pInstancePrivateName, [MarshalAs(UnmanagedType.LPWStr)] string pInstanceSharedName, int dwFlags);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBStartInstance([MarshalAs(UnmanagedType.LPWStr)] string pInstanceName, int dwFlags, [MarshalAs(UnmanagedType.LPWStr)][Out] StringBuilder wszSqlConnection, ref int lpcchSqlConnection);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBStartTracing();
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBStopInstance([MarshalAs(UnmanagedType.LPWStr)] string pInstanceName, int dwFlags, int ulTimeout);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBStopTracing();
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int LocalDBUnshareInstance([MarshalAs(UnmanagedType.LPWStr)] string pInstanceName, int dwFlags);
+  }
+
+  [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+  public struct LocalDbInstanceInfo
+  {
+    internal static readonly int MarshalSize = Marshal.SizeOf(typeof(LocalDbInstanceInfo));
+    public uint Size;
+
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = UnmanagedLocalDbApi.MaxName)]
+    public string InstanceName;
+    public bool Exists;
+    public bool ConfigurationCorrupted;
+    public bool IsRunning;
+    public uint Major;
+    public uint Minor;
+    public uint Build;
+    public uint Revision;
+    public FILETIME LastStartUtc;
+
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = UnmanagedLocalDbApi.MaxPath)]
+    public string Connection;
+    public bool IsShared;
+
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = UnmanagedLocalDbApi.MaxName)]
+    public string SharedInstanceName;
+
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = UnmanagedLocalDbApi.MaxSid)]
+    public string OwnerSID;
+    public bool IsAutomatic;
+  }
+}
